c9bae0559803f67faf07352fb684818f30d3e882
Vasiliy Golban <vgolban@accesssoftek.com>
2021-05-13 09:03:12 +0300

    Add Allure

--- src/test/java/tests/GithubUiTests.java
+++ src/test/java/tests/GithubUiTests.java
@@ -1,7 +1,13 @@
 package tests;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.DriverHelper;
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -9,8 +15,10 @@ import static com.codeborne.selenide.Condition.have;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 import static org.openqa.selenium.By.linkText;
 
+@Feature("Github UI test")
 public class GithubUiTests {
 
     String issueTitle = "Test Issue Title Here";
@@ -19,13 +27,23 @@ public class GithubUiTests {
     @BeforeAll
     public static void beforeAll() {
         DriverHelper.configureDriver();
+        SelenideLogger.addListener("allure", new AllureSelenide().screenshots(true).savePageSource(true));
     }
 
     @Test
+    @DisplayName("Test how issue body displayed")
+    @Story("Github issues userstory")
+    @Description("Verify issue title and body")
     @Tag("web")
     void GithubIssueUiTest() {
+        step("Open page with issues", () -> {
             open("/testaqa/qaguru_hw18/issues/");
+        });
+        step("Click on issue", () -> {
             $("div[aria-label='Issues']").$(linkText(issueTitle)).click();
+        });
+        step("Verify issue body", () -> {
             $(".comment-body p").should(have(text(issueBody)));
+        });
     }
 }
