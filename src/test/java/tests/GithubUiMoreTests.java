dabe5ce052a5c258d9d4fcb4d7db335f120ee4f2
Vasiliy Golban <vgolban@accesssoftek.com>
2021-05-13 08:21:04 +0300

    Add parallel run support

---
+++ src/test/java/tests/GithubUiMoreTests.java
@@ -0,0 +1,32 @@
+package tests;
+
+import helpers.DriverHelper;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.have;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class GithubUiMoreTests {
+
+    String issueTitle = "Test Issue Title Here";
+    String issueBody = "Some issue description";
+
+    @BeforeAll
+    public static void beforeAll() {
+        DriverHelper.configureDriver();
+    }
+
+    @Test
+    @Tag("web")
+    void GithubIssueUiOtherTest() {
+        open("/testaqa/qaguru_hw18/issues/");
+        $("div[aria-label='Issues']").$(linkText(issueTitle)).click();
+        $(".comment-body p").should(have(text(issueBody)));
+    }
+
+}
